d966201d41769b1b7ad3a791459b882336b5d2e1
Kent Fredric <kentfredric@gmail.com>
2017-03-08 23:42:27 +1300

    v1.001001
     - Bugfix: Avoid test failures with -Ddefault_inc_excludes_dot
     - Drop use of ConfigDumper Util
     - Drop use of Test::KENTNL/dztest
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001000, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1, suggests: +1)
     - develop: +12 ↑3 -4 (recommends: +1, suggests: ↑2)
     - runtime: -1 (recommends: +1)
     - test: +1 ↓1 -1 (recommends: ↑1 ↓1)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled/All.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled/All.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::MatchInstalled::All;
 
-our $VERSION = '1.001001';
+our $VERSION = '1.001002';
 
 # ABSTRACT: Upgrade ALL your dependencies to the ones you have installed.
 
